8dd212981fe0267820b397324f213d6f0d8478ff
dianamw <32761660+dianamw@users.noreply.github.com>
2018-01-08 17:34:44 +0100

    Update custom-js.js

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -1,11 +1,12 @@
-function openNav() {
+/*function openNav() {
     document.getElementById("sidebar").style.width = "280px";
     document.getElementById("main").style.marginLeft = "280px";
 }
 function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-}
+}*/
+
 $(document).ready(function () {
     var hash = location.hash;
 	  if (hash){
